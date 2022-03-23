15b827f402fa049d40916f815d767d4a3c2b6d47
Ray Ortiz <oray@bgsu.edu>
2022-03-23 15:12:22 -0400

    Lesson 18 - Form1 updated

--- Lesson18Delegates/Form1/Form1.vb
+++ Lesson18Delegates/Form1/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AddHandler DataGridView1.CellContentClick, AddressOf CellClick
+    End Sub
+    Private Sub CellClick(s As Object, e As DataGridViewCellEventArgs)
+        Dim message = $"Column Index: {e.ColumnIndex}" + vbCr + $"Column Index: {e.RowIndex}"
+        MessageBox.Show(message)
+    End Sub
 End Class
