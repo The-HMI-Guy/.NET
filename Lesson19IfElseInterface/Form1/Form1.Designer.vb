58b59f666a3926e8970421d2fcce02712a5add18
Ray Ortiz <oray@bgsu.edu>
2022-03-28 15:39:23 -0400

    Lesson 19 - If Else

--- Lesson19IfElseInterface/Form1/Form1.Designer.vb
+++ Lesson19IfElseInterface/Form1/Form1.Designer.vb
@@ -22,10 +22,44 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.chkCalendar = New System.Windows.Forms.CheckBox()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.SuspendLayout()
+        '
+        'chkCalendar
+        '
+        Me.chkCalendar.AutoSize = True
+        Me.chkCalendar.Location = New System.Drawing.Point(24, 23)
+        Me.chkCalendar.Margin = New System.Windows.Forms.Padding(15, 14, 15, 14)
+        Me.chkCalendar.Name = "chkCalendar"
+        Me.chkCalendar.Size = New System.Drawing.Size(266, 67)
+        Me.chkCalendar.TabIndex = 0
+        Me.chkCalendar.Text = "Calendar"
+        Me.chkCalendar.UseVisualStyleBackColor = True
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(13, 108)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 1
+        Me.MonthCalendar1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(31.0!, 61.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1924, 1061)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.chkCalendar)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(15, 14, 15, 14)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents chkCalendar As CheckBox
+    Friend WithEvents MonthCalendar1 As MonthCalendar
 End Class
