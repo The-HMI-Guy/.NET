39db6e4464deeffc60ac2d7805e8955938411061
Ray Ortiz <oray@bgsu.edu>
2022-03-30 08:17:58 -0400

    Form 1

--- Lesson21Instance/Form1/Form1.Designer.vb
+++ Lesson21Instance/Form1/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 26)
+        Me.Button1.Location = New System.Drawing.Point(13, 27)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
