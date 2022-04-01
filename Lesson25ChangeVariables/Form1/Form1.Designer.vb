043828691c8a7353ab0bd396ad9b9c65381e994b
Ray Ortiz <oray@bgsu.edu>
2022-04-01 15:09:41 -0400

    small changes

--- Lesson25ChangeVariables/Form1/Form1.Designer.vb
+++ Lesson25ChangeVariables/Form1/Form1.Designer.vb
@@ -22,10 +22,42 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(9, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(140, 55)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Show"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label1.Location = New System.Drawing.Point(6, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(2, 15)
+        Me.Label1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Lesson25ChangeVariables/Form1/Form1.vb
+++ Lesson25ChangeVariables/Form1/Form1.vb
@@ -1,3 +1,14 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Result:"
+        Dim x As Double = 1
 
+        Label1.Text += vbCr + $"x={x}"
+        x = x + 1
+        Label1.Text += vbCr + $"x+1={x}"
+        x *= 2
+        Label1.Text += vbCr + $"x*2={x}"
+        x -= 1
+        Label1.Text += vbCr + $"x-1={x}"
+    End Sub
 End Class
