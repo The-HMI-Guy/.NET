b035bbfc070b4377a3c6b57ead66161d044a08b1
Ray O <oray@bgsu.com>
2022-04-13 15:17:45 -0400

    Lesson 26 - While Stream

---
+++ Lesson26WhileStream/Form1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Imports System.IO
+Public Class Read
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        Dim stream As StreamReader = File.OpenText(OpenFileDialog1.FileName)
+        While stream.Peek() > -1
+            Label1.Text += vbCr + stream.ReadLine()
+        End While
+    End Sub
+End Class
