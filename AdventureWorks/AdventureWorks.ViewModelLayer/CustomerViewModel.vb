7ad3616fc949826d44ac3c6e64fb16723fba8b9b
Ray Ortiz <oray@bgsu.edu>
2023-04-25 11:29:58 -0400

    CustomerViewModel updated.

--- AdventureWorks/AdventureWorks.ViewModelLayer/CustomerViewModel.vb
+++ AdventureWorks/AdventureWorks.ViewModelLayer/CustomerViewModel.vb
@@ -1,3 +1,16 @@
-﻿Public Class CustomerViewModel
+﻿Imports AdventureWorks.EntityLayer
+Imports Common.Library
+Public Class CustomerViewModel
 
+    Sub New()
+        LoadCustomer(1)
+    End Sub
+
+    Public Property Entity As Customer
+
+    Function LoadCustomer(ByVal customerID As Integer) As Customer
+        'Hard-code an entity
+        Entity = New Customer() With {.CustomerID = 1, .FirstName = "Satoshi", .LastName = "Nakamoto", .CompanyName = "Bitcoin Computer Consultants"}
+        Return Entity
+    End Function
 End Class
